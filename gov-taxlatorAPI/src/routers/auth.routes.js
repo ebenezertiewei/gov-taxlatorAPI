f7198106314e4874d513bd5b6d7f12a5a3bd2d2f
Ebenezer Tiewei <ebenezertiewei@gmail.com>
2026-01-19 18:03:08 +0100

    routes update

--- gov-taxlatorAPI/src/routers/auth.routes.js
+++ gov-taxlatorAPI/src/routers/auth.routes.js
@@ -5,23 +5,20 @@ const authMiddleware = require("../middlewares/authMiddleware");
 
 const router = express.Router();
 
-const protect = require("../middlewares/authMiddleware");
-const authController = require("../controllers/authController");
+/* ================= AUTH USER ================= */
+// GET /api/auth/me
+router.get("/me", authMiddleware, authController.me);
 
-router.get("/me", protect, authController.me);
-
-
-// Auth routes
+/* ================= AUTH ROUTES ================= */
 router.post("/signup", authController.signup);
 router.post("/signin", authController.signin);
-router.post("/signout", authController.signout);
+router.post("/signout", authMiddleware, authController.signout);
 
 router.post("/verifyEmail", authController.verifyEmail);
 router.post("/sendVerificationCode", authController.sendVerificationCode);
 
-// Protected routes
+/* ================= PASSWORD ================= */
 router.post("/changePassword", authMiddleware, authController.changePassword);
-
 router.post("/forgotPassword", authController.forgotPassword);
 router.post("/resetPassword", authController.resetPassword);
 
